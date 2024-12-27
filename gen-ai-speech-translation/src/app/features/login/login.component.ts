32ffbe29f96aed11cff36ac3eef14229a85b8a7e
Jaimin Panna <jaimin.panna@samyak.com>
2024-12-27 10:40:11 +0530

    login redirection and ui related changes

--- gen-ai-speech-translation/src/app/features/login/login.component.ts
+++ gen-ai-speech-translation/src/app/features/login/login.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,9 +12,12 @@ import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } 
 })
 export class LoginComponent {
   loginForm!: FormGroup;
+  showLanguageDropdown = false;
+  languages = ['English', 'Hindi', 'Gujarati'];
 
   constructor(
-    public fb: FormBuilder
+    public fb: FormBuilder,
+    private router : Router,
   ) {
     this.loginForm = this.fb.group({
       userName: ['', [Validators.required]],
@@ -22,6 +26,38 @@ export class LoginComponent {
     })
   }
 
+  onUserTypeChange(userType: string): void {
+    if (userType === 'Receiver') {
+      this.showLanguageDropdown = true;
+      this.loginForm.get('lang')?.setValidators([Validators.required]); // Add required validator for language
+    } else {
+      this.showLanguageDropdown = false;
+      this.loginForm.get('lang')?.clearValidators(); // Clear validators for language
+    }
+    this.loginForm.get('lang')?.updateValueAndValidity(); // Update validation status
+  }
+
+  onLogin(): void {
+    if (this.loginForm.valid) {
+      const userType = this.loginForm.get('userType')?.value;
+      const userName = this.loginForm.get('userName')?.value;
+
+      // if (userName) {
+      //   this.signalRService.login(this.userName);
+      //   this.loggedInUsers = this.signalRService.loggedInUsers;
+      // }
+      if (userType === 'Host') {
+        this.router.navigate(['/host']);
+      } else if (userType === 'Receiver') {
+        this.router.navigate(['/receiver']);
+      } else {
+        // Handle unexpected values
+        console.error('Invalid user type selected');
+      }
+    } else {
+      console.error('Form is invalid');
+    }
+  }
 
   onSubmit() {
 
